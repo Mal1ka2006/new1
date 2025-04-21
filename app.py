4fdfdce79a51af8552da05206d81a57aef664bf1
Malika Axmatova <akhmatovamalika700@gmail.com>
2025-04-21 16:21:10 +0500

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import json, os
 from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = 'secret_key_here'
